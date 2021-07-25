0d25945ac901f8f8e8a5e4c2752e032c78573774
vikassh45 <vikasmudgal4@gmail.com>
2021-07-25 11:10:05 +0530

    First Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into user (userid,upassword,uname,urole) values ('admin','admin','admin','admin');
+insert into user (userid,upassword,uname,urole) values ('admin1','admin1','admin1','user');
